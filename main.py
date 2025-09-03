13b9a46b9b21ead7cc8fda41c73b38ee4c45f637
peterdev22 <95014170+peterdev22@users.noreply.github.com>
2025-09-03 22:33:26 +0930

    moved span processing to func, quote ul and ol processing, code consistency

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 
 HTML_DIR = sys.argv[1]
-RESOURCES_DIR = "Resources"
+RESOURCES_DIR = 'Resources'
 OUTPUT_DIR = sys.argv[2]
 
 # id each 's[INT]' class
@@ -24,83 +24,114 @@ def id_classes(html):
         is_italic = 'font-style: italic;' in declaration
         is_underline = 'underline' in declaration
         is_strikethrough = 'line-through' in declaration
-        style_config = [is_bold, is_italic, is_underline, is_strikethrough]
+        is_coloured = 'color: #' in declaration
+        style_config = [is_bold, is_italic, is_underline, is_strikethrough, is_coloured]
         
-        styles[class_] = style_config # each class will have a 4 item array identifying how the text should be formatted
+        styles[class_] = style_config # each class will have a 5 item array identifying how the text should be formatted
 
     return styles
 
 # format text using its 'id'
 def format_text(id, text):
-    is_bold, is_italic, is_underline, is_strikethrough = id
+    is_bold, is_italic, is_underline, is_strikethrough, is_coloured = id
 
-    if is_strikethrough:
-        text = f"~~{text}~~"
     if is_underline:
-        text = f"<u>{text}</u>" # not a part of markdown, may not format correctly in some apps when combined with other styles
+        text = f'<u>{text}</u>' # not a part of markdown, may not format correctly in some apps when combined with other styles
+    if is_strikethrough:
+        text = f'~~{text}~~' # (extended markdown)
     if is_italic:
-        text = f"*{text}*"
+        text = f'*{text}*'
     if is_bold:
-        text = f"**{text}**"
+        text = f'**{text}**'
+    if is_coloured:
+        text = f'=={text}==' # instead of colouring text, it will be highlighted (extended markdown)
 
     return text
+
+# apply formatting using spans found within an element (p, blockquote, ul or ol)
+def process_spans(element, format_config):
+    element_text = []
+
+    for span in element.find_all('span'):
+        span_text = span.get_text()
+        span_class = span.get('class')[0]
+        valid_class_pattern = r's\d+' # only match s[INT] class names
+
+        if re.match(valid_class_pattern, span_class):
+            format_id = format_config[span_class]
+            element_text.append(format_text(format_id, span_text))
+
+    return ''.join(element_text)
 
 # create output folder
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 print(f"Created folder '{OUTPUT_DIR}'")
 
 # repeat this for all journal entries
-for input_file in glob.glob(os.path.join(HTML_DIR, "*.html")):
-    with open(input_file, "r", encoding="utf-8") as f:
-        html = BeautifulSoup(f, "html.parser")
+for input_file in glob.glob(os.path.join(HTML_DIR, '*.html')):
+    with open(input_file, 'r', encoding='utf-8') as f:
+        html = BeautifulSoup(f, 'html.parser')
 
     # find text formatting styles
     format_config = id_classes(html)
     
     # find date
-    date_fancy = html.find("div", class_="pageHeader").text
-    date_iso = datetime.strptime(date_fancy, "%A %d %B %Y").date().isoformat()
+    date_fancy = html.find('div', class_='pageHeader').text
+    date_iso = datetime.strptime(date_fancy, '%A %d %B %Y').date().isoformat()
 
     # find title
-    title = html.find("span", class_="s2").text
+    title = html.find('span', class_='s2').text
 
     # find body text and format accordingly
-    paragraphs = html.find_all("p", class_="p2")
+    body_elements = html.find_all(['p', 'blockquote', 'ol', 'ul'])
     body = []
 
-    for paragraph in paragraphs:
-        paragraph_text = []
-
-        for span in paragraph.find_all('span'):
-            span_text = span.get_text()
-            span_class = span.get("class")[0]
-            valid_class_pattern = r's\d+' # only match s[INT] class names
-
-            if re.match(valid_class_pattern, span_class):
-                format_id = format_config[span_class]
-                paragraph_text.append(format_text(format_id, span_text))
-
-        body.append(''.join(paragraph_text))
+    for element in body_elements:
+        # paragraphs
+        if element.name == 'p' and element.get('class')[0] == 'p2':
+            paragraph_text = process_spans(element, format_config)
+            body.append(paragraph_text)
+            body.append('')
+        # quotes
+        elif element.name == 'blockquote':
+            quote_text = process_spans(element, format_config)
+            body.append(f'> {quote_text}')
+            body.append('')
+        # ordered lists
+        elif element.name == 'ol':
+            list = element.find_all('li')
+            for i, item in enumerate(list, 1):
+                item_text = process_spans(item, format_config)
+                body.append(f'{i}. {item_text}')
+            body.append('')
+        # unordered lists
+        elif element.name == 'ul':
+            list = element.find_all('li')
+            for item in list:
+                item_text = process_spans(item, format_config)
+                body.append(f'- {item_text}')
+            body.append('')
+        
 
     # ----------------- output ----------------------
     md = []
-    filename = f"{date_iso}.md"
+    filename = f'{date_iso}.md'
 
     # yaml properties (for use with obsidian)
-    md.append("---")
-    md.append("date: " + date_iso)
-    md.append("---" + "\n")
+    md.append('---')
+    md.append('date: ' + date_iso)
+    md.append('---' + '\n')
 
     # title & body
-    md.append("# " + title)
+    md.append('# ' + title)
 
-    for paragraph in body:
-        md.append(paragraph + "\n")
+    for line in body:
+        md.append(line)
 
     # write to file
     output_file = os.path.join(OUTPUT_DIR, filename)
-    with open(output_file, "w", encoding="utf-8") as f:
-        f.write("\n".join(md))
+    with open(output_file, 'w', encoding='utf-8') as f:
+        f.write('\n'.join(md))
     # ------------------------------------------------
 
     print(f"Converted '{OUTPUT_DIR}/{date_iso}.md'")
